f10b381c262d7efacaf380c5a80a3ec555044404
Maciej <maciejsmusz@gmail.com>
2017-05-17 16:37:36 -0600

    Got the image on the screen

--- Game/game.rb
+++ Game/game.rb
@@ -1,2 +1,24 @@
-class Game
+require 'rubygems'
+require 'gosu'
+require 'chipmunk'
+
+$background_image = Gosu::Image.new('../assets/images/bg.jpg', :tileable => false, :retro => true)
+$window_x = 640
+$window_y = 480
+module ZOrder
+  Background= *0..1
 end
+
+class GameWindow < Gosu::Window
+  def initialize
+    super $window_x, $window_y
+
+    self.caption = ""
+
+  end
+  def draw
+    $background_image.draw_as_quad(0, 0, 0xffffffff, $window_x, 0, 0xffffffff, $window_x, $window_y, 0xffffffff, 0, $window_y, 0xffffffff, 0)
+  end
+end
+window = GameWindow.new
+window.show
