ae1f96c3caee43ddc56b60064c8f317962ed1281
Maciej <maciejsmusz@gmail.com>
2017-05-17 16:43:44 -0600

    Got rid of chipmunk req and ZOrder

--- Game/game.rb
+++ Game/game.rb
@@ -1,21 +1,18 @@
 require 'rubygems'
 require 'gosu'
-require 'chipmunk'
 
 $background_image = Gosu::Image.new('../assets/images/bg.jpg', :tileable => false, :retro => true)
 $window_x = 640
 $window_y = 480
-module ZOrder
-  Background= *0..1
-end
 
 class GameWindow < Gosu::Window
+
   def initialize
     super $window_x, $window_y
-
-    self.caption = ""
+    self.caption = "Reggorf"
 
   end
+
   def draw
     $background_image.draw_as_quad(0, 0, 0xffffffff, $window_x, 0, 0xffffffff, $window_x, $window_y, 0xffffffff, 0, $window_y, 0xffffffff, 0)
   end
@@ -26,5 +23,6 @@ class GameWindow < Gosu::Window
     end
   end
 end
+
 window = GameWindow.new
 window.show
