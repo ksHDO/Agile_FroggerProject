af7d1ada75d0c9a5833023bc028dd39bc7c7f5fe
Maciej <maciejsmusz@gmail.com>
2017-05-18 16:01:20 -0600

    Added Client and Server code

---
+++ Game/echoserver.rb
@@ -0,0 +1,41 @@
+
+require 'bundler/setup'
+require 'celluloid/autostart'
+require 'celluloid/io'
+
+class EchoServer
+  include Celluloid::IO
+  finalizer :finalize
+
+  def initialize(host, port)
+    puts "*** Starting echo server on #{host}:#{port}"
+
+    # Since we included Celluloid::IO, we're actually making a
+    # Celluloid::IO::TCPServer here
+    @server = TCPServer.new(host, port)
+    async.run
+  end
+
+  def finalize
+    @server.close if @server
+  end
+
+  def run
+    loop {
+      async.handle_connection @server.accept
+    }
+  end
+
+  def handle_connection(socket)
+    _, port, host = socket.peeraddr
+    puts "*** Received connection from #{host}:#{port}"
+    loop { socket.write socket.readpartial(4096)}
+  rescue EOFError
+    puts "*** #{host}:#{port} disconnected"
+    socket.close
+  end
+end
+
+supervisor = EchoServer.supervise('127.0.0.1', 1234)
+trap("INT") { supervisor.terminate; exit }
+sleep
--- Game/game.rb
+++ Game/game.rb
@@ -1,16 +1,42 @@
 require 'rubygems'
 require 'gosu'
+require 'celluloid/io'
+require 'socket'
+require 'securerandom'
+
+include Gosu
 
 $background_image = Gosu::Image.new('../assets/images/bg.jpg', :tileable => false, :retro => true)
 $window_x = 640
 $window_y = 480
 
-class GameWindow < Gosu::Window
+class EchoClient
+  include Celluloid::IO
 
+  def initialize(host, port)
+    puts "*** Connecting to echo server on #{host}:#{port}"
+
+    # This is actually creating a Celluloid::IO::TCPSocket
+    @socket = TCPSocket.new(host, port)
+  end
+
+  def echo(s)
+    @socket.write(s)
+    @socket.readpartial(4096)
+  end
+end
+
+class GameWindow < Window
   def initialize
     super $window_x, $window_y
     self.caption = "Reggorf"
+    # @spritesheet = Image.load_tiles(self, SPRITESHEET, 33, 33, true)
+    # @map = Map.new(self, MAPFILE)  # map representing the movable area
+    @client = EchoClient.new("127.0.0.1", 1234)
+    puts @client.echo("TEST FOR ECHO")
 
+    # @player = player # player name
+    # @font = Font.new(self, 'Courier New', 20)  # for the player names
   end
 
   def draw
