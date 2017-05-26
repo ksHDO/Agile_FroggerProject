772829b81fe6ca5ef63a2211ee2139a029dceb75
Maciej <maciejsmusz@gmail.com>
2017-05-26 16:15:45 -0600

    Added Collision and vehicle

--- Game/game.rb
+++ Game/game.rb
@@ -22,8 +22,9 @@ class GameWindow < Window
     @button2 = Button.new(275,30,Gosu::Image.new('../assets/images/button2.png', :tileable => false, :retro => true))
     @button3 = Button.new(475,30,Gosu::Image.new('../assets/images/button3.png', :tileable => false, :retro => true))
     @button4 = Button.new(675,30,Gosu::Image.new('../assets/images/button4.png', :tileable => false, :retro => true))
-    @player = Player.new(100, $window_y-20)
-    @collision = CollisionDetection.new(Array.[](@player))
+    @frog_player = FrogPlayer.new($window_x*rand, $window_y-20)
+    @vehicle_player = VehiclePlayer.new(@button1)
+    @collision = CollisionDetection.new(Array.[](@frog_player))
     # @font = Font.new(self, 'Courier New', 20)  # for the player names
   end
 
@@ -31,10 +32,22 @@ class GameWindow < Window
     # must update collision first
     @collision.update
 
-    @player.update
+    @frog_player.update
+    @vehicle_player.update
+
+    @vehicle_player.cur_vehicles.each do |car|
+      if car.x < 0
+        @vehicle_player.cur_vehicles.delete(car)
+      end
+      if @frog_player.collides_with(car)
+        @frog_player = FrogPlayer.new($window_x*rand, $window_y-20)
+        @vehicle_player.cur_vehicles.delete(car)
+      end
+    end
     if Input.button_pressed(Gosu::MS_LEFT)
       if @button1.intersects(self.mouse_x, self.mouse_y)
         @button1.on_click
+        @vehicle_player.cur_vehicles.push(Vehicle.new)
       end
       if @button2.intersects(self.mouse_x, self.mouse_y)
         @button2.on_click
@@ -46,18 +59,18 @@ class GameWindow < Window
         @button4.on_click
       end
     end
-
     # must update input last
     Input.update
   end
 
   def draw
     $background_image.draw_as_quad(0, 0, 0xffffffff, $window_x, 0, 0xffffffff, $window_x, $window_y, 0xffffffff, 0, $window_y, 0xffffffff, 0)
-    @player.draw
+    @frog_player.draw
     @button1.draw
     @button2.draw
     @button3.draw
     @button4.draw
+    @vehicle_player.draw
   end
 
   def needs_cursor?
--- Game/player.rb
+++ Game/player.rb
@@ -2,29 +2,13 @@ require 'gosu'
 require '../Game/input'
 require '../Game/collidable'
 
-class Player < Collidable
+
+class Player
+  attr_accessor :x, :y
+
   def initialize(x, y)
-    @image = Gosu::Image.new("../assets/images/gorf.png")
-    super(x, y, @image)
     @x = x
     @y = y
-    @angle = 0.0
-  end
-
-  def update
-    if Input.button_pressed(Gosu::KB_W) or Input.button_pressed(Gosu::Button::KbUp)
-      turn_up
-      move
-    elsif Input.button_pressed(Gosu::KB_A) or Input.button_pressed(Gosu::Button::KbLeft)
-      turn_left
-      move
-    elsif Input.button_pressed(Gosu::KB_S) or Input.button_pressed(Gosu::Button::KbDown)
-      turn_down
-      move
-    elsif Input.button_pressed(Gosu::KB_D) or Input.button_pressed(Gosu::Button::KbRight)
-      turn_right
-      move
-    end
   end
 
   def draw
@@ -57,6 +41,85 @@ class Player < Collidable
     @x += _dx
     @y += _dy
 
-    @aabb.update(_dx, _dy)
+      # @aabb.update(_dx, _dy)
+  end
+end
+
+class FrogPlayer < Player
+  attr_accessor :angle, :image
+
+  def initialize(x, y)
+    @image = Gosu::Image.new("../assets/images/gorf.png")
+    @angle = 0.0
+    super(x, y)
+  end
+
+  def update
+    if Input.button_pressed(Gosu::KB_W) or Input.button_pressed(Gosu::Button::KbUp)
+      turn_up
+      move
+    elsif Input.button_pressed(Gosu::KB_A) or Input.button_pressed(Gosu::Button::KbLeft)
+      turn_left
+      move
+    elsif Input.button_pressed(Gosu::KB_S) or Input.button_pressed(Gosu::Button::KbDown)
+      turn_down
+      move
+    elsif Input.button_pressed(Gosu::KB_D) or Input.button_pressed(Gosu::Button::KbRight)
+      turn_right
+      move
+    end
+  end
+
+  def collides_with(car)
+    if ((@x + 48  >= car.x) && (@x <= car.x + 128)) && ((@y + 48 >= car.y) && (@y <= car.y + 128))
+      true
+    else
+      false
+    end
+  end
+end
+
+class VehiclePlayer < Player
+  attr_accessor :button, :cur_vehicles
+
+  def initialize(button)
+    @button = button
+    @cur_vehicles = []
+  end
+
+  def update
+    move
+  end
+
+  def draw
+    @cur_vehicles.each do |car|
+      car.draw
+    end
+  end
+
+  def move
+    @cur_vehicles.each do |car|
+      car.x -= 10
+    end
+  end
+
+end
+
+class Vehicle
+  attr_accessor :x, :y
+
+  def initialize
+    @x = 1800
+    @y = 800 * rand
+    @image = Gosu::Image.new('../assets/images/car.png')
+  end
+
+  def draw
+    @image.draw(@x, @y, 1)
+  end
+
+  def move
+    @x_location += Gosu.offset_x(@angle, 5)
+    @y_location += Gosu.offset_y(@angle, 5)
   end
 end
